772b2e08a6b651f1fd0cd566b1c00dd06af6d1f0
mikext <wxtmike@outlook.com>
2019-06-03 18:03:00 -0700

    finished draft comparison between Kalman Filter and Linear model

--- KF_draft.m
+++ KF_draft.m
@@ -3,15 +3,15 @@ clear;clc;close all;
 load('../data/cartesian_states.mat');
 load('../data/unbox_LMPs.mat');
 %% use first subject's data
-states_s = cartesian_states{1};
-obsers_s = unbox_LMPs{1};
+states_s = cartesian_states{2};
+obsers_s = unbox_LMPs{2};
 
 states = [states_s.Px states_s.Py states_s.Vx states_s.Vy];
 obsers = obsers_s.LMP;
 %% split data to train slices and test slice
 [N_bin, N_chan] = size(obsers);
 K = 5;
-kk = 1;
+kk = 5;
 n_trial = K - 1;
 N_bin_slice = floor(N_bin/K);
 
@@ -37,6 +37,28 @@ for ss = 1:K
         train_slices{train_count, 1} = slice_s;
     end    
 end
+
+%% train and test a linear model
+X_train = zeros(N_bin_slice * n_trial, N_chan);
+y_train = zeros(N_bin_slice * n_trial, 4);
+for ff = 1:n_trial
+    start_ind = N_bin_slice * (ff - 1) + 1;
+    end_ind = N_bin_slice * ff;
+    X_train(start_ind:end_ind, :) = train_slices{ff}.obsers;
+    y_train(start_ind:end_ind, :) = train_slices{ff}.states;
+end
+X_val = test_slices{1}.obsers;
+y_val = test_slices{1}.states;
+
+% Linear model
+mdl_x = fitlm(X_train, y_train(:, 1));
+y_pred_x = predict(mdl_x, X_val);
+
+mdl_y = fitlm(X_train, y_train(:, 2));
+y_pred_y = predict(mdl_y, X_val);
+
+Px_linear_real = y_val(:, 1);
+Px_linear_pred = y_pred_x;
 %% train parmaters of Kalman Filter
 %% calculate A
 term_1 = zeros(4, 4);
@@ -130,3 +152,47 @@ end
 R = term_1 ./ term_2;
 
 %% Evaluate model on test set
+states = (test_slices{1}.states)';
+obsers = (test_slices{1}.obsers)';
+n_bin = size(obsers, 2);
+
+pred_states = zeros(4, n_bin);
+pred_states(:, 1) = states(:, 1);
+
+mu_0 = states(:, 1);
+sigma_0 = zeros(4, 4);
+
+% iterative updates
+
+for tt = 2:n_bin
+    x_t = obsers(:, tt);
+
+    if tt == 2
+        mu_1 = mu_0;
+        sigma_1 = sigma_0;
+    else
+        mu_1 = mu_3;
+        sigma_1 = sigma_3;
+    end
+
+    mu_2 = A * mu_1;
+    sigma_2 = A * sigma_1 * A.' + Q;
+    K = sigma_2 * C.' / (C * sigma_2 * C.' + R);
+    mu_3 = mu_2 + K * (x_t - C * mu_2);
+    sigma_3 = sigma_2 - K * C * sigma_2;
+
+    pred_states(:, tt) = mu_3;
+end
+%%
+Px_KF_pred = pred_states(1, :);
+Px_KF_real = states(1, :);
+
+%%
+plot(Px_KF_real); hold on; plot(Px_linear_real + 10000); hold off;
+
+%% comparison
+plot(Px_KF_real, 'k'); 
+hold on; 
+plot(Px_KF_pred, 'b'); 
+plot(Px_linear_pred, 'm');
+hold off;
