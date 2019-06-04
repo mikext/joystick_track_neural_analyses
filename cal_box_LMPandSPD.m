9856096c1672f2be6ee53545d77696f392e051ad
mikext <wxtmike@outlook.com>
2019-06-03 21:32:14 -0700

    calculated smoothed LMP and SPD features.

---
+++ cal_box_LMPandSPD.m
@@ -0,0 +1,13 @@
+clear; clc; close all;
+%%
+box_LMPandSPD = cell(4, 1);
+subs = ["fp", "gf", "rh", "rr"];
+for ind = 1:4
+    sub = char(subs(ind));
+    filepath = ['../data/raw/', sub, '_joystick.mat'];
+    load(filepath, 'data');
+    features = feature_extract_paper_method_smo(data);
+    ss = struct('sub', sub, ...
+                'features', features);
+    box_LMPandSPD{ind, 1} = ss;
+end
